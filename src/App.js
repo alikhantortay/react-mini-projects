ad5ec588fc9b2f0f0f63b3772b9556f9dacd4cc9
Алихан Тортай <tortayalikhan@gmail.com>
2023-01-12 17:41:11 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useState } from 'react';
 import './index.scss';
+
+const Modal = ({ open, setOpen }) => (
+   <div className={`overlay animated ${open ? 'show' : ''}`}>
+      <div className="modal">
+         <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+         </svg>
+         <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+      </div>
+   </div>
+)
 
 
 function App() {
 
-   const [count, setCount] = useState(0);
-
-   const onClickPlus = () => {
-      setCount(count + 1)
-   }
-
-   const onClickMinus = () => {
-      setCount(count - 1)
-   }
+   const [open, setOpen] = useState(false)
 
 
    return (
       <div className="App">
-         <div>
-            <h2>Счетчик:</h2>
-            <h1>{count}</h1>
-            <button onClick={onClickMinus} className="minus">- Минус</button>
-            <button onClick={onClickPlus} className="plus">Плюс +</button>
-         </div>
+         <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
+
+         { /* open && (
+            <div className="overlay">
+               <div className="modal">
+                  <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+                     <title />
+                     <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+                  </svg>
+                  <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+               </div>
+            </div>
+         ) */ }
+
+         <Modal open={open} setOpen={setOpen} />
+
       </div>
    );
 }
